158107d9edc25400b2647d0f491a74fc3a987f0e
Coker Richard <coker1229@gmail.com>
2024-02-24 10:37:12 +0100

    Docker ubuntu script update

--- install-docker-compose.sh
+++ install-docker-compose.sh
@@ -14,9 +14,7 @@ sudo apt-get update
 
 # Install docker-compose
    sudo apt-get update
-   sudo apt-get install docker-ce docker-ce-cli containerd.io -y
-   sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-   sudo chmod +x /usr/local/bin/docker-compose
+   sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 # Add ubuntu user into docker group
     sudo usermod -a -G docker ubuntu
